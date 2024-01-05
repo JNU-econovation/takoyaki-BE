a4c84402b5c6c7ac10d15e983c4bb56890f17a68
coco3x <inferior3x@gmail.com>
2024-01-05 15:23:32 +0900

    fix: @Component -> @Repository

--- src/main/java/com/bestbenefits/takoyaki/repository/UserRepository.java
+++ src/main/java/com/bestbenefits/takoyaki/repository/UserRepository.java
@@ -4,10 +4,11 @@ import com.bestbenefits.takoyaki.config.properties.oauth.OAuthSocialType;
 import com.bestbenefits.takoyaki.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
-@Component
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findUserById(Long id);
     Optional<User> findUserByEmailAndSocial(String Email, OAuthSocialType social);
