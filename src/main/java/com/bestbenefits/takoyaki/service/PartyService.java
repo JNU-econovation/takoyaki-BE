3431889252723a7fc2969b82a230a6125733aabf
coco3x <inferior3x@gmail.com>
2024-01-06 19:55:59 +0900

    add: 팟 리스트 조회 추가

--- src/main/java/com/bestbenefits/takoyaki/service/PartyService.java
+++ src/main/java/com/bestbenefits/takoyaki/service/PartyService.java
@@ -2,23 +2,24 @@ package com.bestbenefits.takoyaki.service;
 
 import com.bestbenefits.takoyaki.DTO.client.request.PartyCreationReqDTO;
 import com.bestbenefits.takoyaki.DTO.client.response.PartyCreationResDTO;
-import com.bestbenefits.takoyaki.config.apiresponse.ApiResponse;
+import com.bestbenefits.takoyaki.DTO.client.response.PartyListForLoginUserResDTO;
+import com.bestbenefits.takoyaki.DTO.client.response.PartyListResDTO;
 import com.bestbenefits.takoyaki.config.properties.party.ActivityLocation;
 import com.bestbenefits.takoyaki.config.properties.party.Category;
-import com.bestbenefits.takoyaki.config.properties.party.ContactMethod;
-import com.bestbenefits.takoyaki.config.properties.party.DurationUnit;
 import com.bestbenefits.takoyaki.entity.Party;
 import com.bestbenefits.takoyaki.entity.User;
 import com.bestbenefits.takoyaki.repository.PartyRepository;
 import com.bestbenefits.takoyaki.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
-import java.util.Optional;
-
-import static com.bestbenefits.takoyaki.config.properties.party.DurationUnit.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -48,5 +49,63 @@ public class PartyService {
             throw new IllegalArgumentException("이미 삭제된 Party입니다.");
 
         p.updateDeleteAt(LocalDateTime.now());
+    }
+
+    @Transactional(readOnly = true)
+    public List<PartyListResDTO> getParties(int number, int pageNumber, Category category, ActivityLocation activityLocation){
+        List<Object[]> partyList = partyRepository.getPartiesByFiltering(PageRequest.of(pageNumber, number), category, activityLocation).getContent();
+        List<PartyListResDTO> partyDTOList = new ArrayList<>();
+
+        for (Object[] row : partyList) {
+            int recruitNumber = (int) row[4];
+            int waitingNumber = ((Long) row[6]).intValue();
+            int acceptedNumber = ((Long) row[7]).intValue();
+            float competitionRate = (waitingNumber != 0) ? (float) (recruitNumber - acceptedNumber)/waitingNumber : 0f;
+            partyDTOList.add(
+                    PartyListResDTO.builder()
+                            .partyId((Long) row[0])
+                            .title((String) row[1])
+                            .category(((Category) row[2]).getName())
+                            .activityLocation(((ActivityLocation) row[3]).getName())
+                            .recruitNumber(recruitNumber)
+                            .plannedClosingDate((LocalDate) row[5])
+                            .waitingNumber(waitingNumber)
+                            .acceptedNumber(acceptedNumber)
+                            .competitionRate(competitionRate)
+                            .build()
+            );
+        }
+        return partyDTOList;
+    }
+
+    @Transactional(readOnly = true)
+    public List<PartyListForLoginUserResDTO> getParties(Long id, int number, int pageNumber, Category category, ActivityLocation activityLocation){
+        User user = userRepository.findUserById(id).orElseThrow(
+                ()->new IllegalArgumentException("유저가 없습니다."));
+
+        List<Object[]> partyList = partyRepository.getPartiesWithBookmarkFlagByFiltering(user, PageRequest.of(pageNumber, number), category, activityLocation).getContent();
+        List<PartyListForLoginUserResDTO> partyDTOList = new ArrayList<>();
+
+        for (Object[] row : partyList) {
+            int recruitNumber = (int) row[4];
+            int waitingNumber = ((Long) row[6]).intValue();
+            int acceptedNumber = ((Long) row[7]).intValue();
+            float competitionRate = (waitingNumber != 0) ? (float) (recruitNumber - acceptedNumber)/waitingNumber : 0f;
+            partyDTOList.add(
+                    PartyListForLoginUserResDTO.builder()
+                            .partyId((Long) row[0])
+                            .title((String) row[1])
+                            .category(((Category) row[2]).getName())
+                            .activityLocation(((ActivityLocation) row[3]).getName())
+                            .recruitNumber(recruitNumber)
+                            .plannedClosingDate((LocalDate) row[5])
+                            .waitingNumber(waitingNumber)
+                            .acceptedNumber(acceptedNumber)
+                            .competitionRate(competitionRate)
+                            .bookmarked((boolean) row[8])
+                            .build()
+            );
+        }
+        return partyDTOList;
     }
 }
