8f43a8ff1289d62354dfd3f64bf4d6094725b9f0
coco3x <inferior3x@gmail.com>
2024-01-07 23:55:36 +0900

    refactor: 팟 신청 서비스

--- src/main/java/com/bestbenefits/takoyaki/service/YakiService.java
+++ src/main/java/com/bestbenefits/takoyaki/service/YakiService.java
@@ -15,19 +15,18 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 public class YakiService {
     private final UserRepository userRepository;
-    private final PartyRepository partyRepository;
+    private final UserService userService;
+    private final PartyService partyService;
     private final YakiRepositoy yakiRepositoy;
 
     @Transactional
     public void applyToParty(Long id, Long partyId){
-        User user = userRepository.findUserById(id).orElseThrow(() -> new IllegalArgumentException("유저가 없습니다."));
-        Party party = partyRepository.findById(partyId)
-                .filter(p -> p.getDeletedAt() == null)
-                .filter(p -> p.getCreatedAt().isEqual(p.getClosedAt()))
-                .orElseThrow(() -> new IllegalArgumentException("존재하지 않거나 마감된 팟입니다."));
+        User user = userService.getUserOrThrow(id);
+        Party party = partyService.getAvailableParty(partyId);
         if (yakiRepositoy.existsYakiByPartyAndUser(party, user))
             throw new IllegalArgumentException("이미 신청한 팟입니다.");
         yakiRepositoy.save(new Yaki(user, party));
     }
+
 
 }
