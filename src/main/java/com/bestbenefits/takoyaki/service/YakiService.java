a562f9f75e3dd95bedb7eb0ef29b1980222f3251
coco3x <inferior3x@gmail.com>
2024-01-08 00:21:59 +0900

    fix: 팟 검증 코드 수정

--- src/main/java/com/bestbenefits/takoyaki/service/YakiService.java
+++ src/main/java/com/bestbenefits/takoyaki/service/YakiService.java
@@ -22,7 +22,9 @@ public class YakiService {
     @Transactional
     public void applyToParty(Long id, Long partyId){
         User user = userService.getUserOrThrow(id);
-        Party party = partyService.getAvailableParty(partyId);
+        Party party = partyService.getPartyOrThrow(partyId);
+        if (party.isDeleted() && party.isClosed())
+            throw new IllegalArgumentException("존재하지 않거나 삭제된 팟입니다.");
         if (yakiRepositoy.existsYakiByPartyAndUser(party, user))
             throw new IllegalArgumentException("이미 신청한 팟입니다.");
         yakiRepositoy.save(new Yaki(user, party));
