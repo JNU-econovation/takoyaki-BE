0369e3d11aefd0f7d55f2db152b91f59228484e1
coco3x <inferior3x@gmail.com>
2024-01-07 20:23:53 +0900

    add: Yaki 레포지토리 추가

---
+++ src/main/java/com/bestbenefits/takoyaki/repository/YakiRepositoy.java
@@ -0,0 +1,23 @@
+package com.bestbenefits.takoyaki.repository;
+
+import com.bestbenefits.takoyaki.DTO.client.response.PartyYakiListResDTO;
+import com.bestbenefits.takoyaki.entity.Party;
+import com.bestbenefits.takoyaki.entity.User;
+import com.bestbenefits.takoyaki.entity.Yaki;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface YakiRepositoy extends JpaRepository<Yaki, Long> {
+    Optional<Yaki> findYakiByPartyAndUser(Party party, User user);
+
+    @Query("SELECT y.user.id, y.user.nickname FROM Yaki y WHERE y.party = :party AND y.status = 'WAITING'")
+    List<PartyYakiListResDTO> findWaitingList(Party party);
+
+    @Query("SELECT y.user.id, y.user.nickname FROM Yaki y WHERE y.party = :party AND y.status = 'ACCEPTED'")
+    List<PartyYakiListResDTO> findAcceptedList(Party party);
+}
