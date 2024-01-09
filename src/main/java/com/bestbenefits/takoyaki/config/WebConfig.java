1bbcd17a09d2a9fc2143d9716a9aab5506474800
coco3x <inferior3x@gmail.com>
2024-01-09 23:52:20 +0900

    fix: CORS

--- src/main/java/com/bestbenefits/takoyaki/config/WebConfig.java
+++ src/main/java/com/bestbenefits/takoyaki/config/WebConfig.java
@@ -45,9 +45,10 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("*")
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH")
-                .allowedHeaders("*");
+                .allowedOrigins("http://localhost:8080")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH")//, "OPTIONS"
+                .allowedHeaders("*")
+                .allowCredentials(true);
     }
 
 }
