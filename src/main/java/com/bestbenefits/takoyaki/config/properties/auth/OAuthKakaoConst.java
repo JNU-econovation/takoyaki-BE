96013292813d29ef3e46584276ec890c4abf086a
Jangan Lee <lja3723@gmail.com>
2024-01-23 02:20:53 +0900

    fix: redirect url

--- src/main/java/com/bestbenefits/takoyaki/config/properties/auth/OAuthKakaoConst.java
+++ src/main/java/com/bestbenefits/takoyaki/config/properties/auth/OAuthKakaoConst.java
@@ -5,7 +5,7 @@ public final class OAuthKakaoConst {
     public static final String URL2 = "https://kapi.kakao.com";
     public static final String AUTHORIZATION_CODE_URI = "/oauth/authorize";
     public static final String ACCESS_TOKEN_URI = "/oauth/token";
-    public static final String REDIRECT_URI = "http://localhost:8080/oauth";
+    public static final String REDIRECT_URI = "https://api.takoyaki.store/oauth";
     public static final String AGREEMENT_URI = "/v2/user/scopes";
     public static final String USER_INFO_URI = "/v2/user/me";
     public static final String LOGOUT_URI = "/v1/user/logout";
